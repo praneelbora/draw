d772015efaf86c9bfe6aef5c1e60f9f0517087a5
Gaurish <gaurishbaliga@gmail.com>
2022-11-15 00:09:39 +0530

    Add Basic UI

---
+++ colors.py
@@ -0,0 +1,13 @@
+# Declaration of program color palette.
+white = (255, 255, 255)
+purple = (94, 33, 235)
+green = (136, 255, 85)
+blue = (0, 0, 128)
+hovergreen = (208, 240, 192)
+black = (0,0,0)
+darkpurple = (48, 25, 52)
+red = (255, 87, 87)
+offwhite = (240,240,240)
+background = (92,225,230)
+buttonColor = (3,152,158)
+buttonColorBottom = (0,0,0)
--- draw.py
+++ draw.py
@@ -1,5 +1,22 @@
 import pygame,sys
+import colors
 # '''
+
+pygame.init()
+
+class Rectangle:
+    '''Class for creating rectangles
+    with border radius throughout program'''
+    def __init__(self,width,height,pos ,bgcolor):  #creating class button and listing arguments
+        #top rectangle
+        self.top_rect = pygame.Rect(pos,(width,height))
+        self.top_color = (bgcolor)     
+
+    def draw(self):
+        '''draw function of class button'''
+        pygame.draw.rect(screen,self.top_color,self.top_rect,border_radius = 20)
+
+
 class Button:
 	def __init__(self,text,width,height,pos,elevation):
 		#Core attributes 
@@ -10,33 +27,33 @@ class Button:
 
 		# top rectangle 
 		self.top_rect = pygame.Rect(pos,(width,height))
-		self.top_color = (250,150,150)
+		self.top_color =  (3,152,158)
 
 		# bottom rectangle 
 		self.bottom_rect = pygame.Rect(pos,(width,height))
-		self.bottom_color = (150,150,150)
+		self.bottom_color = (0,0,0)
 		#text
-		# self.text_surf = gui_font.render(text,True,(255,255,255))
-		# self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
+		self.text_surf = gui_font.render(text,True,(255,255,255))
+		self.text_rect = self.text_surf.get_rect(center = self.top_rect.center)
 
 	def draw(self):
 
 		# elevation logic 
 		self.top_rect.y = self.original_y_pos - self.dynamic_elecation
-		# self.text_rect.center = self.top_rect.center 
+		self.text_rect.center = self.top_rect.center 
 
 		self.bottom_rect.midtop = self.top_rect.midtop
 		self.bottom_rect.height = self.top_rect.height + self.dynamic_elecation
 
-		pygame.draw.rect(screen,self.bottom_color, self.bottom_rect,border_radius = 12)
-		pygame.draw.rect(screen,self.top_color, self.top_rect,border_radius = 12)
-		# screen.blit(self.text_surf, self.text_rect)
+		pygame.draw.rect(screen,self.bottom_color, self.bottom_rect,border_radius = 15)
+		pygame.draw.rect(screen,self.top_color, self.top_rect,border_radius = 15)
+		screen.blit(self.text_surf, self.text_rect)
 		self.check_click()
 
 	def check_click(self):
 		mouse_pos = pygame.mouse.get_pos()
 		if self.top_rect.collidepoint(mouse_pos):
-			self.top_color = (250,150,150)
+			self.top_color =  (3,152,158)
 			if pygame.mouse.get_pressed()[0]:
 				self.dynamic_elecation = 0
 				self.pressed = True
@@ -47,7 +64,7 @@ class Button:
 					self.pressed = False
 		else:
 			self.dynamic_elecation = self.elevation
-			self.top_color = (250,150,150)
+			self.top_color =  (3,152,158)
 # '''
 WIDTH, HEIGHT = 1280,720
 BG = (255,255,255)
@@ -56,21 +73,23 @@ clock = pygame.time.Clock()
 
 FPS=60
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
-# gui_font = pygame.font.SysFont('rockwell',30)
+gui_font = pygame.font.SysFont('rockwell',30)
 
-button1 = Button('Click me',200,40,(200,250),5)
-# BACK=
+#buttons
+button1 = Button('Fill',220,60,(1000,70),5)
+button2 = Button('Select Colour',220,60,(1000,170),5)
+button3 = Button('Straight Line',220,60,(1000,270),5)
+button4 = Button('Draw',220,60,(1000,370),5)
+button5 = Button('Undo',100,60,(1000,470),5)
+button6 = Button('Redo',100,60,(1120,470),5)
 
-# def draw():
-    # GAME.blit(BACK,(0,0))
-
-    # pygame.display.update()
-
+#rectangles
+rectangle1 = Rectangle(900, 630, (50, 50), colors.white)
+Img = pygame.image.load('Draw!.png')
 
 def main():
     running = True
-    screen.fill(BG)
-    screen.fill((255,255,255))
+    screen.fill(colors.background)
     while running:
         clock.tick(60)
         for event in pygame.event.get():
@@ -79,6 +98,16 @@ def main():
         # draw()
         
         button1.draw()
+        button2.draw()
+        button3.draw()
+        button4.draw()
+        button5.draw()
+        button6.draw()
+        rectangle1.draw()
+        screen.blit(Img, (1060,570))
+
+
+
         pygame.display.update()
     pygame.quit()
 
