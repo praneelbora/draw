ea9a497d9fb0fdebd81125f31fadc486928a118e
jagjitsbhumra <jagjit.s.bhumra@gmail.com>
2022-11-28 21:20:19 +0530

    Several changes done (check desc.)
     - Added straight line function, works but preview isn't enabled (which should actually be fine)
     - Tried to add flood fill but is not working (to be checked)
     - Button clicking needs to be fixed

--- draw.py
+++ draw.py
@@ -1,10 +1,42 @@
 import pygame,sys
 import colors
+import copy
+import ujson, json
+# wfile = open("JsonExample.json", "r+")
+#rfile = open("JsonExample.json", "r")
+# t = ujson.dumps(graph)
+# print(t)
 # '''
 
 pygame.init()
 
 position = tuple()
+
+def flood_recursive(matrix, x, y, start_color, color_to_update):
+	width = len(matrix)
+	height = len(matrix[0])
+	def fill(x,y,start_color,color_to_update):
+		#if the square is not the same color as the starting point
+		if matrix[x][y] != start_color:
+			return
+		#if the square is not the new color
+		elif matrix[x][y] == color_to_update:
+			return
+		else:
+			#update the color of the current square to the replacement color
+			matrix[x][y] = color_to_update
+			neighbors = [(x-1,y),(x+1,y),(x-1,y-1),(x+1,y+1),(x-1,y+1),(x+1,y-1),(x,y-1),(x,y+1)]
+			for n in neighbors:
+				if 0 <= n[0] <= width-1 and 0 <= n[1] <= height-1:
+					fill(n[0],n[1],start_color,color_to_update)
+	fill(x,y,start_color,color_to_update)
+	#pick a random starting point
+	# start_x = random.randint(0,width-1)
+	# start_y = random.randint(0,height-1)
+	# start_color = matrix[start_x][start_y]
+	# fill(start_x,start_y,start_color,9)
+	return matrix
+
 
 class Rectangle:
 	'''Class for creating rectangles
@@ -112,14 +144,8 @@ rectangle1 = Rectangle(900, 630, (50, 50), colors.white)
 surf = screen.subsurface((49, 49, 902, 632))
 Img = pygame.image.load('Draw!.png')
 
-graph = [[(255, 255, 255) for i in range(WIDTH)] for j in range(HEIGHT)]
-undo = list()
-undo.append([[(255, 255, 255) for i in range(WIDTH)] for j in range(HEIGHT)])
-for j in range(HEIGHT):
-	for i in range(WIDTH):
-		if undo[-1][j][i] != (255,255,255):
-			print('ok',i,j)
-redo = list()
+graph = [[screen.get_at((i,j)) for i in range(WIDTH)] for j in range(HEIGHT)]
+
 
 print(len(graph[0]))
 
@@ -133,7 +159,7 @@ def roundline(srf, color, start, end, radius):
 		for j in range(-1*radius,radius):
 			for k in range(-1*radius,radius):
 				if (abs(j+k)<abs(2*radius-1)):
-					graph[y+k][x+j] = 1
+					# graph[y+k][x+j] = 1
 					srf.set_at((x+j-49, y+k-49), color)
 
 
@@ -142,7 +168,12 @@ colours = [colour1, colour2, colour3, colour4, colour5]
 	
 def main():
 	DefaultColour = BLACK
+	drawing = True
 	draw_on = False
+	fill = False
+	filling = False
+	lining = False
+	line = False
 	last_pos = (0, 0)
 	color = (255, 128, 0)
 	radius = 3
@@ -153,9 +184,20 @@ def main():
 
 	# running = True
 	screen.fill(colors.background)
+	rectangle1.draw()
+	screen.blit(Img, (1060,570))
+
+	for button in buttons:
+		button.draw()
+	undo = list()
+	undo.append([[screen.get_at((i,j)) for i in range(WIDTH)] for j in range(HEIGHT)])
+	# for j in range(HEIGHT):
+	# 	for i in range(WIDTH):
+	# 		if undo[-1][j][i] != (255,255,255):
+	# 			print('ok',i,j)
+	redo = list()
 	# drawing = False
 	thickness = 10
-	rectangle1.draw()
 	pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
 	counter = 0
 
@@ -187,31 +229,35 @@ def main():
 		# global DefaultColour
 		# print(DefaultColour)
 		print(len(undo))
-		if(len(undo) <=  1):
+		if(len(undo) <= 1):
+			# for j in range(650):
+			# 	for i in range(902):
+			# 		surf.set_at((i,j), (255,255,255))
 			return
+			# screen.fill(92,225,230)
 		else:
-			global graph, surf
+			# global graph, surf
 			# redo.append(graph)
 			undo.pop()
 			print(len(undo))
 			# undo.pop(-1)
 			# print(len(undo))
-			graph = undo[-1]
+			# graph = undo[-1]
 
-			for j in range(HEIGHT):
-				for i in range(WIDTH):
-					if undo[-1][j][i] != (255,255,255):
-						print(i,j)
+			# for j in range(HEIGHT):
+			# 	for i in range(WIDTH):
+			# 		if undo[-1][j][i] != (255,255,255):
+			# 			print(i,j)
 
 			for i in range(HEIGHT):
 				for j in range(WIDTH):
 					# if(graph[i][j] != (255, 255, 255)):
-						surf.set_at((j - 49,i - 49), undo[-1][i][j])
+						screen.set_at((j,i), undo[-1][i][j])
 
-			for j in range(HEIGHT):
-				for i in range(WIDTH):
-					if undo[-1][j][i] != (255,255,255):
-						print(i,j)
+			# for j in range(HEIGHT):
+			# 	for i in range(WIDTH):
+			# 		if undo[-1][j][i] != (255,255,255):
+			# 			print(i,j)
 
 			# pygame.display.update()
 
@@ -231,15 +277,27 @@ def main():
 				button.draw()
 
 			if buttons[0].checking():
+				drawing = False
+				lining = False
+				filling = True
 				pass
 			
 			if buttons[1].checking():
+				for col in colours:
+					col.draw()
+				clock.tick(10000)
 				DefaultColour = setColour()
 
 			if buttons[2].checking():
+				drawing = False
+				filling = False
+				lining = True
 				pass
 
 			if buttons[3].checking():
+				filling = False
+				lining = False
+				drawing = True
 				pass
 
 			if buttons[4].checking():
@@ -259,25 +317,45 @@ def main():
 				raise StopIteration
 				
 			if e.type == pygame.MOUSEBUTTONDOWN:
-				color = DefaultColour
-				for j in range(-1*radius,radius):
-					for k in range(-1*radius,radius):
-						if (abs(j+k)<abs(2*radius-1)):
-							graph[e.pos[1]+k][e.pos[0]+j] = color
-							surf.set_at((e.pos[0]+j-49, e.pos[1]+k-49), color)
-
-				draw_on = True
+				if draw_on:
+					color = DefaultColour
+					for j in range(-1*radius,radius):
+						for k in range(-1*radius,radius):
+							if (abs(j+k)<abs(2*radius-1)):
+								# graph[e.pos[1]+k][e.pos[0]+j] = color
+								surf.set_at((e.pos[0]+j-49, e.pos[1]+k-49), color)
+				elif fill:
+					color = DefaultColour
+					graph = flood_recursive(graph, e.pos[0], e.pos[1], screen.get_at(e.pos), color)
+					for i in range(HEIGHT):
+						for j in range(WIDTH):
+								screen.set_at((j,i), graph[i][j])
+				last_pos1 = e.pos
+				
+				if ((e.pos[1] < 650) and (e.pos[0] < 930) and (drawing==True)):
+					draw_on = True
+				elif  ((e.pos[1] < 650) and (e.pos[0] < 930) and (filling==True)):
+					fill = True
+				elif  ((e.pos[1] < 650) and (e.pos[0] < 930) and (lining==True)):
+					line = True
 			if e.type == pygame.MOUSEBUTTONUP:
+				if line:
+					color = DefaultColour
+					roundline(surf, color, e.pos, last_pos1, radius)
+
+				if ((last_pos[1] < 650) and (last_pos[0] < 930) and (last_pos1[0]<650) and (last_pos1[1]<930)):
+					undo.append([[screen.get_at((i,j)) for i in range(WIDTH)] for j in range(HEIGHT)])
+							
 				draw_on = False
-				if ((e.pos[1] < 650) and (e.pos[0] < 930)):
-					undo.append(graph.copy())
+				fill = False
+				line = False
 			if e.type == pygame.MOUSEMOTION:
 				if draw_on:
 					color = DefaultColour
 					for j in range(-1*radius,radius):
 						for k in range(-1*radius,radius):
 							if (abs(j+k)<abs(2*radius-1)):
-								graph[e.pos[1]+k][e.pos[0]+j] = color
+								# graph[e.pos[1]+k][e.pos[0]+j] = color
 								surf.set_at((e.pos[0]+j-49, e.pos[1]+k-49), color)
 
 					roundline(surf, color, e.pos, last_pos, radius)
